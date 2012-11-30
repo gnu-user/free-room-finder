a1f782d88cd91cb99afc094166ccc0d1f1f0c904
minou.jhin@hotmail.com <jhin1234>
2012-11-30 18:09:33 -0500

    test comment added

--- templates/header.php
+++ templates/header.php
@@ -42,6 +42,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <!-- The title of the page -->
     <title>Free Room Finder</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Free Room Finder">
