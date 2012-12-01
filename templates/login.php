d48bb2f988dc79b2b158d01f4cf59cc45daa787a
Wesley Unwin <wes_unwin@hotmail.com>
2012-12-01 15:46:01 -0500

    few more corrections to login.php...

--- templates/login.php
+++ templates/login.php
@@ -1,42 +1,48 @@
 
-<form class="form-horizontal">
+</br>
+<h1> Welcome to the Free Room Finder! </h1>
+</br></br>
 
-	<!-- Username label and textbox -->
-	<div class="control-group">
-		<label class="control-label" for="inputEmail">User name:</label>				
-		<div class="controls">
-			<input type="text" id="login_username" name="login_username" maxlength="31" pattern="^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$" placeholder="Username">      		
-		</div>
-	</div>
-	
-	<!-- Password label and textbox -->
-	<div class="control-group">
-		<label class="control-label" for="inputPassword">Password</label>
-		<div class="controls">
-			<input type="password" id="login_password" name="login_password" maxlength="31" pattern="^[a-zA-Z0-9\!\$\%\^\&amp;\*\(\)\_\?]{6,31}$" placeholder="Password">
-		</div>
-	</div>
-
-	<!-- Remember me label and checkbox -->
-	<div class="control-group">
-		<div class="controls">
-			<label class="checkbox">
-				<input type="checkbox" id="login_remember" name="login_remember" checked="checked" value="1" > Remember me
-			</label>
-			<button type="submit" id="LoginButton" name="LoginButton" class="btn">Sign in</button>
-		</div>
-	</div>
-	
-	<!-- 'Not a member...' and register button' -->
-	<div class="control-group">
-		<div class="controls">
-
-		</div>
-	</div>			
+<div>
+	<form class="form-horizontal" >
+		<h2> Please sign in... </h2> </br>
 		
-	
-</form>
+		<!-- Username label and textbox -->
+		<div class="control-group">
+			<label class="control-label" for="inputEmail">User name:</label>				
+			<div class="controls">
+				<input type="text" id="login_username" name="login_username" maxlength="31" pattern="^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$" placeholder="Username">      		
+			</div>
+		</div>
+		
+		<!-- Password label and textbox -->
+		<div class="control-group">
+			<label class="control-label" for="inputPassword">Password</label>
+			<div class="controls">
+				<input type="password" id="login_password" name="login_password" maxlength="31" pattern="^[a-zA-Z0-9\!\$\%\^\&amp;\*\(\)\_\?]{6,31}$" placeholder="Password">
+			</div>
+		</div>
 
+		<!-- Remember me label and checkbox -->
+		<div class="control-group">
+			<div class="controls">
+				<label class="checkbox">
+					<input type="checkbox" id="login_remember" name="login_remember" checked="checked" value="1" > Remember me
+				</label>
+				<button type="submit" id="LoginButton" name="LoginButton" class="btn">Sign in</button>
+			</div>
+		</div>
+		
+		<!-- 'Not a member...' and register button' -->
+		<div class="control-group">
+			<div class="controls">
+
+			</div>
+		</div>			
+			
+		
+	</form>
+</div>
 
 
 
