d7285752402023111cf9996d90630daa99e488b5
gnu_user <gsoc.student@gmail.com>
2012-12-02 00:54:43 -0500

    Fixed the header template for logged in users to display calendar properly

--- templates/header-user.php
+++ templates/header-user.php
@@ -60,6 +60,7 @@
     <!-- CSS styles -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/datepicker.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
