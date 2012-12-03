d877987931144b320e215673fe9ee88c85cd95ce
gnu_user <gsoc.student@gmail.com>
2012-12-03 03:46:52 -0500

    Added the statistics page

--- statistics.php
+++ statistics.php
@@ -31,6 +31,11 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+require_once "inc/free_room_auth.php";
+require_once "inc/db_interface.php";
+require_once "inc/validate.php";
+require_once "inc/verify.php";
+require_once "inc/utility.php";
 
 session_start();
 
@@ -41,4 +46,62 @@ session_start();
  * This main page refers to (if logged out) login.php or (if logged in) room_request.php
  * 
  */
+
+ 
+/* Connect to the database */
+$mysqli_conn = new mysqli("localhost", $db_user, $db_pass, $db_name);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
+
+
+/* 1. If the user is not logged in or their session is invalid */
+if (verify_login_cookie($mysqli_conn, $SESSION_KEY) === false
+	&& (!isset($_SESSION['login'])
+			|| verify_login_session($mysqli_conn, $_SESSION['login'], $SESSION_KEY) === false))
+{
+	/* Redirect the user to the login page */
+	header('Location: login.php');
+}
+
+/* User has a valid login cookie set / has logged into the site with valid account
+ * and the POST data isset */
+elseif (verify_login_cookie($mysqli_conn, $SESSION_KEY)
+		|| verify_login_session($mysqli_conn, $_SESSION['login'], $SESSION_KEY))
+{
+	/* FIX, forgot to account for when user has login cookie set but there is no session
+	 * data, have to retrieve username from cookie and then set the session data
+	*/
+	if (verify_login_cookie($mysqli_conn, $SESSION_KEY))
+	{
+		/* Get the login cookie data */
+		$login_cookie = htmlspecialchars($_COOKIE['login']);
+
+		/* Get the username from login cookie data and set session info */
+		$username = username_from_session($mysqli_conn, $login_cookie, $SESSION_KEY);
+		set_session_data($mysqli_conn, $username, $SESSION_KEY);
+	}
+}
+
+/* Invalid cookie or session data/etc.. */
+else
+{
+	/* Redirect the user to the login page */
+	header('Location: login.php');
+}
+
+
+/* Display the statistics page */
+include 'templates/header-user.php';
+
+include 'templates/statistics.php';
+
+/* Include the footer */
+include 'templates/footer.php';
+
+
+
 ?>
