92a048eb654a310db99a5018f14d3a90fceb5b65
gnu_user <gsoc.student@gmail.com>
2012-12-03 17:53:19 -0500

    Added the download helper script, it's not used as ZIP compression is not supported

---
+++ inc/download.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ *  Free Room Finder Website
+ *
+ *
+ *  Authors -- Crow's Foot Group
+ *  -------------------------------------------------------
+ *
+ *  Jonathan Gillett
+ *  Joseph Heron
+ *  Amit Jain
+ *  Wesley Unwin
+ *  Anthony Jihn
+ *
+ *
+ *  License
+ *  -------------------------------------------------------
+ *
+ *  Copyright (C) 2012 Crow's Foot Group
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * A helper PHP script which ensures that files are downloaded properly on various
+ * browsers, instead of the download button opening a separate page
+ */
+$file = $_GET['file'];
+$type = $_GET['type'];
+header("Content-type: application/xml");
+header("Content-Disposition: attachment; filename=". $file);
+readfile($file);
+?>
