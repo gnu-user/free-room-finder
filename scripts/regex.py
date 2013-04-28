cf0297029fd7842a089f07397c982d341c09b42b
gnu_user <gsoc.student@gmail.com>
2013-04-27 23:55:49 -0400

    Fixed the parsing issues for spring/summer semester

--- scripts/regex.py
+++ scripts/regex.py
@@ -12,10 +12,10 @@ import re
 re_course_info= re.compile(r'(^.*)\s*\-\s*(\d{5})\s*\-\s*([\w]{2,4})\s*(\d{4}[\w]{1})\s*\-*\s*(\d{0,3})')
 
 # Matches course term info, groups: school, term, year
-re_course_term = re.compile(r'^([A-Z]*)\s(\w*)\s(\d{4})')
+re_course_term = re.compile(r'^([A-Z]*)\s([\w/]*)\s(\d{4})')
 
 # Matches the course level info, ie. Post Secondary, Undergraduate, Graduate, etc.
-re_course_level = re.compile(r'\s*(Post\sSecondary|Undergraduate|Graduate|Professional)\s*')
+re_course_level = re.compile(r'\s*(Post\sSecondary|Continuing\sEducation|Undergraduate|Graduate|Professional)\s*')
 
 # Matches the prof name, groups: first name, last name
 #re_prof_name = re.compile(r'([A-Z]\w*|[\w\s\.\'\,\-]*[A-Z]\w*)\s\(|(TBA)')
