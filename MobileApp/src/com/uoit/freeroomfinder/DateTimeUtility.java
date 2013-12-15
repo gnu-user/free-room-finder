5b19357e5d72997cfbc429e701474ec7aab1de16
Joseph <heron.joseph@gmail.com>
2013-12-15 15:48:05 -0500

    ADDED: more documentation

--- MobileApp/src/com/uoit/freeroomfinder/DateTimeUtility.java
+++ MobileApp/src/com/uoit/freeroomfinder/DateTimeUtility.java
@@ -20,6 +20,11 @@ public class DateTimeUtility
     private Locale locale;
     private boolean notUseArmyClock;
 
+    /**
+     * Create a DateTimeUtility
+     * @param notUseArmyClock Whether to use 12 hour or 24 hour format
+     * @param locale The local of the time.
+     */
     public DateTimeUtility(boolean notUseArmyClock, Locale locale)
     {
         if (notUseArmyClock)
@@ -37,6 +42,10 @@ public class DateTimeUtility
         this.notUseArmyClock = notUseArmyClock;
     }
 
+    /**
+     * Change time format
+     * @param notUseArmyClock Whether to use 12 hour clock or not
+     */
     public void setArmyClock(boolean notUseArmyClock)
     {
         if (this.notUseArmyClock != notUseArmyClock)
@@ -54,31 +63,63 @@ public class DateTimeUtility
         }
     }
 
+    /**
+     * Format the date
+     * @param date The date
+     * @return The formatted date
+     */
     public String formatDate(Date date)
     {
         return sdf.format(date);
     }
 
+    /**
+     * Format the date
+     * @param date The date
+     * @return The formatted date
+     */
     public String formatDate(long date)
     {
         return sdf.format(date);
     }
 
+    /**
+     * Format the time
+     * @param date The date
+     * @return The formatted time
+     */
     public String formatTime(Date date)
     {
         return stf.format(date);
     }
 
+    /**
+     * Format the time
+     * @param date The formatted date
+     * @return The formatted time
+     */
     public String formatFullTime(String time) throws ParseException
     {
         return stfFull.format(parseTime(time));
     }
 
+    /**
+     * Parse the date
+     * @param date The formatted date
+     * @return The date
+     * @throws ParseException Parsing exception from the given string
+     */
     public Date parseDate(String date) throws ParseException
     {
         return sdf.parse(date);
     }
 
+    /**
+     * Parse the time
+     * @param time The formatted time
+     * @return The time
+     * @throws ParseException Parsing exception from the given string
+     */
     public Date parseTime(String time) throws ParseException
     {
         return stf.parse(time);
