75e1cc71a647b061afc49c3de6cbfc040de92259
gnu_user <gsoc.student@gmail.com>
2013-12-15 00:05:46 -0500

    Added support to format the time in the format needed by REST api

--- MobileApp/src/com/uoit/freeroomfinder/DateTimeUtility.java
+++ MobileApp/src/com/uoit/freeroomfinder/DateTimeUtility.java
@@ -5,74 +5,81 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class DateTimeUtility {
+public class DateTimeUtility
+{
+    private static final String ARMY_TIME = "kk:mm";
+    private static final String NORMAL_TIME = "hh:mm aa";
+    private static final String FULL_TIME = "hh:mm:ss";
+    private static final String DATE = "yyyy-MM-dd";
 
-	private static final String ARMY_TIME = "kk:mm";
-	private static final String NORMAL_TIME = "hh:mm aa";
-	private static final String DATE = "yyyy-MM-dd";
-	
-	private SimpleDateFormat stf;
-	private SimpleDateFormat sdf;
-	
-	private Locale locale;
-	private boolean notUseArmyClock;
-	
-	public DateTimeUtility(boolean notUseArmyClock, Locale locale)
-	{
-		if(notUseArmyClock)
-		{
-			stf = new SimpleDateFormat(NORMAL_TIME, locale);
-		}
-		else
-		{
-			stf = new SimpleDateFormat(ARMY_TIME, locale);
-		}
-		sdf = new SimpleDateFormat(DATE, locale);
-		
-		this.locale = locale;
-		this.notUseArmyClock = notUseArmyClock;
-	}
-	
-	public void setArmyClock(boolean notUseArmyClock)
-	{
-		if(this.notUseArmyClock != notUseArmyClock)
-		{
-			this.notUseArmyClock = notUseArmyClock;
-			
-			if(notUseArmyClock)
-			{
-				stf = new SimpleDateFormat(NORMAL_TIME, locale);
-			}
-			else
-			{
-				stf = new SimpleDateFormat(ARMY_TIME, locale);
-			}
-		}
-	}
-	
-	public String formatDate(Date date)
-	{
-		return sdf.format(date);
-	}
-	
-	public String formatDate(long date)
-	{
-		return sdf.format(date);
-	}
-	
-	public Date parseDate(String date) throws ParseException
-	{
-		return sdf.parse(date);
-	}
-	
-	public Date parseTime(String time) throws ParseException
-	{
-	    return stf.parse(time);
-	}
-	
-	public String formatTime(Date date)
-	{
-		return stf.format(date);
-	}
-	
+    private SimpleDateFormat stf;
+    private SimpleDateFormat stfFull;
+    private SimpleDateFormat sdf;
+
+    private Locale locale;
+    private boolean notUseArmyClock;
+
+    public DateTimeUtility(boolean notUseArmyClock, Locale locale)
+    {
+        if (notUseArmyClock)
+        {
+            stf = new SimpleDateFormat(NORMAL_TIME, locale);
+        }
+        else
+        {
+            stf = new SimpleDateFormat(ARMY_TIME, locale);
+        }
+        stfFull = new SimpleDateFormat(FULL_TIME, locale);
+        sdf = new SimpleDateFormat(DATE, locale);
+
+        this.locale = locale;
+        this.notUseArmyClock = notUseArmyClock;
+    }
+
+    public void setArmyClock(boolean notUseArmyClock)
+    {
+        if (this.notUseArmyClock != notUseArmyClock)
+        {
+            this.notUseArmyClock = notUseArmyClock;
+
+            if (notUseArmyClock)
+            {
+                stf = new SimpleDateFormat(NORMAL_TIME, locale);
+            }
+            else
+            {
+                stf = new SimpleDateFormat(ARMY_TIME, locale);
+            }
+        }
+    }
+
+    public String formatDate(Date date)
+    {
+        return sdf.format(date);
+    }
+
+    public String formatDate(long date)
+    {
+        return sdf.format(date);
+    }
+
+    public String formatTime(Date date)
+    {
+        return stf.format(date);
+    }
+
+    public String formatFullTime(String time) throws ParseException
+    {
+        return stfFull.format(parseTime(time));
+    }
+
+    public Date parseDate(String date) throws ParseException
+    {
+        return sdf.parse(date);
+    }
+
+    public Date parseTime(String time) throws ParseException
+    {
+        return stf.parse(time);
+    }
 }
